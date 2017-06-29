a8d5141262ed577d254beb8ba8ea0a3aa71ec774
Seun Matt <smatt382@gmail.com>
2017-06-29 06:34:03 +0100

    update travis script

--- src/config/ConfigHelper.php
+++ src/config/ConfigHelper.php
@@ -12,6 +12,16 @@ namespace Config;
 class ConfigHelper {
 
     public static function config() {
+        //first try to read access_token from the env
+        $access_token = getenv("access_token");
+        if($access_token) {
+            return [
+                "access_token" => $access_token,
+                "base_url" => "https://www.formstack.com/api/v2/"
+                ];
+        }
+
+        //if not set in the env, try looking for a config file
         if(!file_exists(realpath("./formstack.php")) ) {
             throw new \Exception("ERROR: config file [formstack.php] not found in package root's dir. 
             \nFile is expected here in this dir:
